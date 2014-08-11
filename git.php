de8d3d34fe0a575ce81c2030315adae573d7a9da
Gastón Acosta <correo@gaston.com.uy>
2014-08-11 17:17:35 -0300

    first commit

---
+++ git.php
@@ -0,0 +1,39 @@
+<?php
+
+$branch = "qa";
+$log = "/var/www/qa/logs/git.log";
+
+$repos = array(
+	"bunker" => ".",
+	"bunker-ng" => "api/v2",
+	"bunker-analytics-etl" => "bunker-analytics-etl",
+	"bunker-nbs" => "bunker-nbs"
+);
+
+$payload = json_decode($_POST['payload']);
+
+if(isset($payload)){
+
+	__log("==== BEGIN Payload from " . $payload->repository->name );
+	$chdir = $repos[$payload->repository->name];
+	exec("cd ${chdir} &&  git reset --hard HEAD && git pull origin +${branch} 2>&1", $output);
+	__log($output);
+	__log("==== PHINX");
+	exec("php -f ". __FILE__ ."/vendor/robmorgan/phinx/bin/phinx migrate 2>&1", $output);
+	__log($output);
+	__log("==== END");
+
+}else{
+	__log("Dummy call received from " . $_SERVER['REMOTE_ADDR']);
+}
+
+function __log($log){
+	$message = date('[m/d/Y h:i:s a] ');
+	if(is_array($log)){
+		$message .= implode( PHP_EOL, $log);
+	}else{
+		$message .= $log;
+	}
+	$message .= PHP_EOL;
+	file_put_contents($log, $message, FILE_APPEND);
+}
