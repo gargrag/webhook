113e9a19de9458e6b6cd90c7bea3d2aae62c933e
Gastón Acosta <gacosta@theelectricfactory.com>
2014-11-12 10:00:50 -0200

    First version

---
+++ WebHook.php
@@ -0,0 +1,124 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use HipChat\HipChat;
+
+class WebHook
+{
+
+    private $config;
+
+    public function __construct()
+    {
+        $this->config = parse_ini_file('config.ini');
+    }
+
+    public function process_payload()
+    {
+
+        if (isset($_POST['payload'])) {
+
+            $payload = json_encode($POST['payload']);
+
+            $this->logfile("=== BEGIN payload from " . $payload->repository->slug);
+
+            foreach ($payload->commits as $commit) {
+                preg_match_all("/merge/i", $commit->message, $matches);
+
+                if (count($matches) > 0) {
+                    $message = "It seems that $commit->author <br/>
+                    has <b>merged</b> smth into $commit->branch on the <b>repo: $payload->repository->name</b>";
+
+                    $this->hipchat_notify($message);
+                }
+            }
+
+            $this->git_pull($payload->repository->slug);
+            $this->post_commands();
+
+            $this->logfile("=== END payload from " . $payload->repository->slug);
+
+        } else {
+
+            $this->logfile("=== EMPTY Call from " . $_SERVER['HTTP_REMOTE_ADDR']);
+        }
+    }
+
+    private function git_pull($repo)
+    {
+
+        $branch = $this->config['repositories']['branch'];
+        $dir = $this->config['repositories'][$repo];
+
+        $command = "cd ${dir} && git pull origin $branch";
+        exec($command, $output, $return_var);
+
+        $this->logfile($output);
+
+        if ($return_var != 0) {
+            $output = implode('<br />', $output);
+            $message = "Can\'t pull on ${repo}::${branch} <br/> ${output}";
+            $this->hipchat_notify("Can\'t pull on ${repo}::${branch} <br/>");
+        }
+
+
+    }
+
+    private function post_commands(){
+        if(count($this->config['post_commands']) > 0){
+            foreach($this->config['post_commands'] as $post_command){
+                exec($post_command, $output, $return_var);
+                $exit_status = ( $return_var == 0 ) ? "succeed" : "fails";
+                $this->logfile("POST COMMAND : ${post_command}, ${exit_status} with output: ");
+                $this->logfile($output);
+
+                if($return_var > 0){
+                    $output = implode('<br />', $output);
+                    $message = "POST COMMAND: ${post_command} failed, with output: <br />" . $output;
+
+                    $this->hipchat_notify($message, "warning");
+                }
+            }
+        }
+    }
+
+
+    private function hipchat_notify($message, $type = null)
+    {
+
+        $hc_config = $this->config['hipchat'];
+
+        switch ($type) {
+            case 'warning' :
+                $color = HipChat::COLOR_YELLOW;
+                break;
+
+            case 'error' :
+                $color = HipChat::COLOR_RED;
+                break;
+
+            default :
+                $color = HipChat::COLOR_GRAY;
+                break;
+        }
+
+        $hc = new HipChat($hc_config['token']);
+        $hc->message_room($hc_config['from'] . $hc_config['room'],
+            $message, false, $color, HipChat::FORMAT_HTML);
+
+    }
+
+    private function logfile($message)
+    {
+
+        if (is_array($message)) {
+            $message .= implode(PHP_EOL, $message);
+        }
+
+        $date = date(DATE_RFC2822);
+        $message = "[${date}] ${message}";
+
+        file_put_contents($this->config['general']['logfile'], $message, FILE_APPEND);
+    }
+
+}
--- git.php
+++ git.php
@@ -1,39 +1,5 @@
 <?php
+require_once 'WebHook.php';
 
-define('TH_BRANCH', 'qa');
-define('TH_LOG_FILE', '/var/www/qa/logs/git.log');
-
-$repos = array(
-	"bunker" => ".",
-	"bunker-ng" => "api/v2",
-	"bunker-analytics-etl" => "bunker-analytics-etl",
-	"bunker-nbs" => "bunker-nbs"
-);
-
-$payload = json_decode($_POST['payload']);
-
-if(isset($payload)){
-
-	__log("==== BEGIN Payload from " . $payload->repository->name );
-	$chdir = $repos[$payload->repository->name];
-	exec("cd ${chdir} &&  git reset --hard HEAD && git pull origin" . TH_BRANCH . "2>&1", $output);
-	__log($output);
-	__log("==== PHINX");
-	exec("php -f ". dirname(__FILE__) ."/vendor/robmorgan/phinx/bin/phinx migrate 2>&1", $output);
-	__log($output);
-	__log("==== END");
-
-}else{
-	__log("Dummy call received from " . $_SERVER['REMOTE_ADDR']);
-}
-
-function __log($log){
-	$message = date('[m/d/Y h:i:s a] ');
-	if(is_array($log)){
-		$message .= implode( PHP_EOL, $log);
-	}else{
-		$message .= $log;
-	}
-	$message .= PHP_EOL;
-	file_put_contents(TH_LOG_FILE, $message, FILE_APPEND);
-}
+$wh = new WebHook();
+$wh->process_payload();
